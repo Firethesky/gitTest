f82e1d5de31e06d486cff1fbb403b78320366a0e
Firethesky <2534729810@qq.com>
2024-11-29 10:38:42 +0800

    rename test/NewClass

---
+++ test/src/main/java/org/example/NewClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class NewClass {
+    public int addTest(int a, int b) {
+        return a + b;
+    }
+    public int subTest(int a, int b){
+        return a - b;
+    }
+}
